9779f349fdac43418d4b07425e180e044b4344cf
Martin Swarbrick <martin.swarbrick@gmail.com>
2021-10-02 14:56:46 +0100

    Updated PlotTableInput for telepath

--- wagtail_plotly/widgets/layout.py
+++ wagtail_plotly/widgets/layout.py
@@ -10,3 +10,9 @@ class LayoutChooser(AdminChooser):
     choose_another_text = _('Choose another plotly layout')
     model = Layout
     choose_modal_url_name = 'plotly_layout_chooser:choose'
+
+    # TODO: How to add edit link
+    # link_to_chosen_text = _('Edit this layout')
+
+    # def get_edit_item_url(self, item):
+    #     return reverse('wagtailsnippets:edit', args=('base', 'people', quote(item.pk)))
