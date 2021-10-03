c9361c410a298816abe3a37a1ed5d69490f59e3b
Martin Swarbrick <martin.swarbrick@gmail.com>
2021-10-03 14:57:48 +0100

    Added edit links for admin palette and layout choosers

--- wagtail_plotly/widgets/layout.py
+++ wagtail_plotly/widgets/layout.py
@@ -3,16 +3,15 @@ from django.utils.translation import gettext_lazy as _
 from generic_chooser.widgets import AdminChooser
 
 from ..models.layout import Layout
+from ..wagtail_hooks import LayoutAdmin
 
 
 class LayoutChooser(AdminChooser):
     choose_one_text = _('Choose a plotly layout')
     choose_another_text = _('Choose another plotly layout')
+    link_to_chosen_text = _('Edit this layout')
     model = Layout
     choose_modal_url_name = 'plotly_layout_chooser:choose'
 
-    # TODO: How to add edit link
-    # link_to_chosen_text = _('Edit this layout')
-
-    # def get_edit_item_url(self, item):
-    #     return reverse('wagtailsnippets:edit', args=('base', 'people', quote(item.pk)))
+    def get_edit_item_url(self, item):
+        return LayoutAdmin().url_helper.get_action_url('edit', item.pk)
